d0816124a9a7762b5a970f4f5dce3888e95165a6
Najla Elmachtoub <nelmachtoub@gmail.com>
2011-04-20 19:40:56 -0400

    Moved drawing of icons to personview and added variable selected

--- GUI/OpenCommGUI/src/edu/cornell/opencomm/PersonView.java
+++ GUI/OpenCommGUI/src/edu/cornell/opencomm/PersonView.java
@@ -1,14 +1,23 @@
 package edu.cornell.opencomm;
 
+import java.util.ListIterator;
+
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Paint.Style;
+import android.graphics.drawable.ShapeDrawable;
+import android.graphics.drawable.shapes.RectShape;
 
 public class PersonView {
 	Context context;
 	Person person;
 	int x, y, w, h;
 	Bitmap image;
+	boolean selected;
 	
 	public PersonView(Context context, Person person, int x, int y, int w, int h){
 		this.context = context;
@@ -17,7 +26,7 @@ public class PersonView {
 		this.y = y;
 		this.w = w;
 		this.h = h;
-		
+		this.selected = false;
 		
 		BitmapFactory.Options opts= new BitmapFactory.Options();
 		opts.inJustDecodeBounds= true;
@@ -50,9 +59,17 @@ public class PersonView {
 		return this.person;
 	}
 	
+	public void changeSelected() {
+		this.selected = !this.selected;
+	}
+	
 	public boolean clickedInside(int mouseX, int mouseY){
 		if((mouseX>=x) && (mouseX<=(x+w)) && (mouseY>=y) && (mouseY<=(y+h)))
 			return true;
 		return false;
+	}
+	
+	protected void draw(Canvas canvas){
+		canvas.drawBitmap(image, x, y, null);
 	}
 }
--- GUI/OpenCommGUI/src/edu/cornell/opencomm/ScreenView.java
+++ GUI/OpenCommGUI/src/edu/cornell/opencomm/ScreenView.java
@@ -5,131 +5,141 @@ import java.util.ListIterator;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Paint.Style;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
 import android.widget.LinearLayout;
 
 public class ScreenView extends LinearLayout {
-	
+
 	public ScreenView(Context context, AttributeSet attrs) {
 		super(context, attrs);
 		this.context = context;
 		setFocusable(true);
 		setFocusableInTouchMode(true);
 	}
-	
-	public ScreenView(Context context, PrivateSpace space){
+
+	public ScreenView(Context context, PrivateSpace space) {
 		super(context);
 		this.context = context;
 		this.space = space;
 		setFocusable(true);
-		setFocusableInTouchMode(true);	
+		setFocusableInTouchMode(true);
 	}
 
 	private Context context;
 	private PrivateSpace space; // the group of people this is displaying
 	private LinkedList<PersonView> icons;
-	int x =0, y=0, w=320, h = 430;
+	int x = 0, y = 0, w = 320, h = 430;
 	private PersonView selectedIcon;
-	int initialX=0, initialY=0; // position of the icon before you moved it
-	// NORA - I guestimated the mainScreenH and adjusted it, we might need a better method for this
-	int mainScreenH = 340; // the height of the main screen (the whole screen height - bar height)
-	PrivateSpace hoveredPrivSpace=null; // the Private Space that is being hovered over
-	
-	
-	
-	public void setSpace(PrivateSpace s){
+	int initialX = 0, initialY = 0; // position of the icon before you moved it
+	// NORA - I guestimated the mainScreenH and adjusted it, we might need a
+	// better method for this
+	int mainScreenH = 340; // the height of the main screen (the whole screen
+							// height - bar height)
+	PrivateSpace hoveredPrivSpace = null; // the Private Space that is being
+											// hovered over
+
+	public void setSpace(PrivateSpace s) {
 		space = s;
 		createIcons(s);
 	}
-	
-	/** Takes the people who are in this private space, and creates an icon (PersonView) for them 
-	 * for only this screen */
-	public void createIcons(PrivateSpace space){
+
+	/**
+	 * Takes the people who are in this private space, and creates an icon
+	 * (PersonView) for them for only this screen
+	 */
+	public void createIcons(PrivateSpace space) {
 		icons = new LinkedList<PersonView>();
 		LinkedList<Person> people = space.getPeople();
-		
-		ListIterator<Person> i= people.listIterator();
-		while(i.hasNext()){
+
+		ListIterator<Person> i = people.listIterator();
+		while (i.hasNext()) {
 			Person p = i.next();
-			icons.add(new PersonView(context, p, (int)(Math.floor(w*Math.random())), 
-					(int)(Math.floor(mainScreenH*Math.random())), 50, 50));
-			i=people.listIterator(i.nextIndex());
+			icons.add(new PersonView(context, p, (int) (Math.floor(w
+					* Math.random())), (int) (Math.floor(mainScreenH
+					* Math.random())), 50, 50));
+			i = people.listIterator(i.nextIndex());
 		}
-		
+
 	}
-	
-	protected void onDraw(Canvas canvas){
+
+	protected void onDraw(Canvas canvas) {
 		// draw background
-		canvas.drawColor(space.getColor()); 
+		canvas.drawColor(space.getColor());
+
 		// draw people icons
-		ListIterator<PersonView> i= icons.listIterator();
-		while(i.hasNext()){
+		ListIterator<PersonView> i = icons.listIterator();
+		while (i.hasNext()) {
+			
 			PersonView icon = i.next();
-			canvas.drawBitmap(icon.getIcon(), icon.getX(), icon.getY(), null);
+			icon.draw(canvas);
+			//canvas.drawBitmap(icon.getIcon(), icon.getX(), icon.getY(), p);
 		}
-	} 
-	
-	public boolean onTouchEvent(MotionEvent event){
+	}
+
+	public boolean onTouchEvent(MotionEvent event) {
 		int eventaction = event.getAction();
-		int mouseX = (int)event.getX();
-		int mouseY = (int)event.getY();
-		
-		
-		switch(eventaction){
-			case MotionEvent.ACTION_DOWN:
-				selectedIcon=null;
-				ListIterator<PersonView> i= icons.listIterator();
-				while(i.hasNext() == true){
-					PersonView icon = i.next();
-					if(icon.clickedInside(mouseX, mouseY)){
-						selectedIcon = icon;
-						initialX = icon.getX();
-						initialY = icon.getY();
-					}
-					i= icons.listIterator(i.nextIndex());
+		int mouseX = (int) event.getX();
+		int mouseY = (int) event.getY();
+
+		switch (eventaction) {
+		case MotionEvent.ACTION_DOWN:
+			selectedIcon = null;
+			ListIterator<PersonView> i = icons.listIterator();
+			while (i.hasNext() == true) {
+				PersonView icon = i.next();
+				if (icon.clickedInside(mouseX, mouseY)) {
+					selectedIcon = icon;
+					initialX = icon.getX();
+					initialY = icon.getY();
 				}
-				break;
-			case MotionEvent.ACTION_MOVE:
-				// If a person icon is selected, then move the icon to the current position
-				if(selectedIcon!=null){
-					selectedIcon.setX(mouseX-(selectedIcon.getW()/2));
-					selectedIcon.setY(mouseY-(selectedIcon.getH()/2));
-					// if icon is dragged over private space, then highlight that private space icon
-					if(hoveredPrivSpace==null){
-						for (PrivateSpace p : PrivateSpace.currentSpaces){
-							if (p.contains(mouseX,mouseY)){
-								p.setHovered(true);
-								hoveredPrivSpace = p;
-							}
+				i = icons.listIterator(i.nextIndex());
+			}
+			break;
+		case MotionEvent.ACTION_MOVE:
+			// If a person icon is selected, then move the icon to the current
+			// position
+			if (selectedIcon != null) {
+				selectedIcon.setX(mouseX - (selectedIcon.getW() / 2));
+				selectedIcon.setY(mouseY - (selectedIcon.getH() / 2));
+				// if icon is dragged over private space, then highlight that
+				// private space icon
+				if (hoveredPrivSpace == null) {
+					for (PrivateSpace p : PrivateSpace.currentSpaces) {
+						if (p.contains(mouseX, mouseY)) {
+							p.setHovered(true);
+							hoveredPrivSpace = p;
 						}
 					}
-					else if (hoveredPrivSpace!=null){
-						if(!hoveredPrivSpace.contains(mouseX, mouseY)){
-							hoveredPrivSpace.setHovered(false);
-							hoveredPrivSpace = null;
-						}
+				} else if (hoveredPrivSpace != null) {
+					if (!hoveredPrivSpace.contains(mouseX, mouseY)) {
+						hoveredPrivSpace.setHovered(false);
+						hoveredPrivSpace = null;
 					}
 				}
-				break;
-			case MotionEvent.ACTION_UP:
-				if(selectedIcon!=null){
-					for (PrivateSpace p : PrivateSpace.currentSpaces){
-						if (p.contains(mouseX,mouseY)){
-							p.add(selectedIcon.getPerson());
-							break;
-//							return true;
-						}
+			}
+			break;
+		case MotionEvent.ACTION_UP:
+			if (selectedIcon != null) {
+				for (PrivateSpace p : PrivateSpace.currentSpaces) {
+					if (p.contains(mouseX, mouseY)) {
+						p.add(selectedIcon.getPerson());
+						break;
+						// return true;
 					}
-					//Draw in bounds
-					if(mouseY>=mainScreenH && mouseY<=h){
-						selectedIcon.setX(initialX);
-						selectedIcon.setY(initialY);
-					}
-					selectedIcon = null;
 				}
-				break;
-		}  
+				// Draw in bounds
+				if (mouseY >= mainScreenH && mouseY <= h) {
+					selectedIcon.setX(initialX);
+					selectedIcon.setY(initialY);
+				}
+				selectedIcon = null;
+			}
+			break;
+		}
 		invalidate();
 		return true;
 	}
