13458c2b191b141c40a1e304231db009047d4b19
Nora N-Q <ncn27@cornell.edu>
2011-04-22 10:15:09 -0400

    Why won't you commit
    Signed-off-by: Nora N-Q <ncn27@cornell.edu>

--- GUI/OpenCommGUI/gen/edu/cornell/opencomm/R.java
+++ GUI/OpenCommGUI/gen/edu/cornell/opencomm/R.java
@@ -11,10 +11,10 @@ public final class R {
     public static final class attr {
     }
     public static final class color {
-        public static final int gray=0x7f040002;
+        public static final int gray=0x7f040001;
         public static final int light_gray=0x7f040000;
         public static final int main_background=0x7f040003;
-        public static final int scroll_background=0x7f040001;
+        public static final int scroll_background=0x7f040002;
     }
     public static final class drawable {
         public static final int icon=0x7f020000;
@@ -33,9 +33,9 @@ public final class R {
         public static final int main=0x7f030000;
     }
     public static final class string {
-        public static final int app_name=0x7f050003;
+        public static final int app_name=0x7f050001;
         public static final int hello=0x7f050000;
-        public static final int main_button_text=0x7f050002;
-        public static final int trash_button_text=0x7f050001;
+        public static final int main_button_text=0x7f050003;
+        public static final int trash_button_text=0x7f050002;
     }
 }
